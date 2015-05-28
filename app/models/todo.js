e9f131697001734da4696711cd89b6b74c53071d
kailevy <kai0levy@gmail.com>
2015-05-28 14:55:17 -0700

    Timestamped and grammatically correct

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Todo', {
-  text : {type : String, default: ''}
+  text : {type : String, default: ''},
+  timestamp: {type: Date, default: Date.now}
 });
