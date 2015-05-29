fc99b31583ede28709f1acfd3368885200a133a1
kailevy <kai0levy@gmail.com>
2015-05-29 16:32:25 -0700

    Changed css

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,5 +2,6 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Todo', {
   text : {type : String, default: ''},
-  timestamp: {type: Date, default: Date.now}
+  timestamp: {type: Date, default: Date.now},
+  priority: {type : Number, default: 0}
 });
